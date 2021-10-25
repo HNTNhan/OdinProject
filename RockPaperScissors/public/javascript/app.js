c3cbc61feb084106aeff69f2c659c8726442dbd0
HNTNhan <HNMFRV@gmail.com>
2021-10-25 09:38:02 +0700

    Add sound when click, won, lost game

--- RockPaperScissors/public/javascript/app.js
+++ RockPaperScissors/public/javascript/app.js
@@ -1,3 +1,55 @@
+let currRound;
+let userScore;
+let computerScore;
+let maxScore;
+let isGameOver;
+
+const userChoose = document.querySelectorAll(".buttons button");
+const imgUserChoose = document.querySelector(".img-user-choose");
+const imgComputerChoose = document.querySelector(".img-computer-choose");
+const showResultContent = document.querySelector(".show-result-content");
+const roundHeader = document.querySelector(".current-round");
+const textRoundResult = document.querySelector(".text-round-result");
+const textUserScore = document.querySelector(".user-score");
+const textComputerScore = document.querySelector(".computer-score");
+const endGameModal = document.querySelector(".end-game-modal");
+const endResult = document.querySelector(".modal-result");
+const restartButton = document.querySelector(".restart-button");
+const newGameButton = document.querySelector(".new-game-button");
+const showMaxScore = document.querySelector(".show-max-score");
+
+function newGame() {
+  maxScore = parseInt(document.querySelector("#max-score").value);
+  maxScore = maxScore < 1 ? 1 : maxScore;
+  currRound = 1;
+  userScore = 0;
+  computerScore = 0;
+  isGameOver = false;
+  textUserScore.textContent = 0;
+  textComputerScore.textContent = 0;
+  endGameModal.style.display = "none";
+  showResultContent.style.display = "none";
+  showMaxScore.textContent = "Max Score: " + maxScore;
+}
+
+function gameOver() {
+  isGameOver = true;
+  endGameModal.style.display = "flex";
+  if (userScore > computerScore) {
+    endResult.textContent = "You Won!";
+    endResult.style.color = "green";
+    playSound("won-sound");
+  } else if (userScore === computerScore) {
+    endResult.textContent = "Tie!";
+    endResult.style.color = "blue";
+    playSound("won-sound");
+  } else {
+    playSound("lost-sound");
+    endResult.textContent = "You Lost!";
+    endResult.style.color = "red";
+  }
+}
+
 function computerPlay() {
   let random = Math.floor(Math.random() * 3);
   if (random === 0) return "rock";
@@ -20,62 +72,6 @@ function checkRound(playerChoose, computerChoose) {
     else if (computerChoose.toLowerCase() === "scissors") return 0;
   } else {
     return "Something went wrong!!!";
-  }
-}
-
-let currRound;
-let userScore;
-let computerScore;
-let maxScore;
-
-const userChoose = document.querySelectorAll(".buttons button");
-const imgUserChoose = document.querySelector(".img-user-choose");
-const imgComputerChoose = document.querySelector(".img-computer-choose");
-const showResultContent = document.querySelector(".show-result-content");
-const roundHeader = document.querySelector(".current-round");
-const textRoundResult = document.querySelector(".text-round-result");
-const textUserScore = document.querySelector(".user-score");
-const textComputerScore = document.querySelector(".computer-score");
-const endGameModal = document.querySelector(".end-game-modal");
-const endResult = document.querySelector(".modal-result");
-const restartButton = document.querySelector(".restart-button");
-const newGameButton = document.querySelector(".new-game-button");
-const showMaxScore = document.querySelector(".show-max-score");
-
-restartButton.addEventListener("click", function () {
-  startNewGame();
-});
-
-newGameButton.addEventListener("click", function () {
-  startNewGame();
-});
-
-function startNewGame() {
-  maxScore = parseInt(document.querySelector("#max-score").value);
-  maxScore = maxScore < 1 ? 1 : maxScore;
-  currRound = 1;
-  userScore = 0;
-  computerScore = 0;
-  textUserScore.textContent = 0;
-  textComputerScore.textContent = 0;
-  endGameModal.style.display = "none";
-  showResultContent.style.display = "none";
-  showMaxScore.textContent = "Max Score: " + maxScore;
-}
-
-startNewGame();
-
-function gameOver() {
-  endGameModal.style.display = "flex";
-  if (userScore > computerScore) {
-    endResult.textContent = "You Won!";
-    endResult.style.color = "green";
-  } else if (userScore === computerScore) {
-    endResult.textContent = "Tie!";
-    endResult.style.color = "blue";
-  } else {
-    endResult.textContent = "You Lost!";
-    endResult.style.color = "red";
   }
 }
 
@@ -109,6 +105,21 @@ function updateSatus(result) {
   }
 }
 
+function playSound(id) {
+  document.querySelector(`#${id}`).currentTime = 0;
+  document.querySelector(`#${id}`).play();
+}
+
+restartButton.addEventListener("click", function () {
+  newGame();
+});
+
+newGameButton.addEventListener("click", function () {
+  newGame();
+});
+
+newGame();
+
 for (let obj of userChoose) {
   obj.addEventListener("click", function () {
     let computerChoose = computerPlay();
@@ -119,19 +130,26 @@ for (let obj of userChoose) {
     imgComputerChoose.children[1].src =
       "public/images/" + computerChoose + ".jpg";
     showResultContent.style.display = "block";
+    playSound("click-sound");
     updateSatus(result);
   });
 }
 
-//Test Case//
-
-function testCheckRound() {
-  console.group("Test Check Round Function");
-  console.log(checkRound("Scissors", "Rock") === -1);
-  console.log(checkRound("Rock", "Rock") === 0);
-  console.log(checkRound("Rock", "Scissors") === 1);
-  console.log(checkRound("Paper", "Rock") === 1);
-  console.log(checkRound("Paper", "Scissors") === -1);
-  console.groupEnd();
-}
-testCheckRound();
+window.addEventListener("keydown", (e) => {
+  if (!isGameOver) {
+    switch (e.key) {
+      case "1":
+        document.querySelector(`.rock-button`).click();
+        playSound("click-sound");
+        break;
+      case "2":
+        document.querySelector(`.paper-button`).click();
+        playSound("click-sound");
+        break;
+      case "3":
+        document.querySelector(`.scissors-button`).click();
+        playSound("click-sound");
+        break;
+    }
+  }
+});
